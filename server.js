26e5eabee2dada88854b31c501f557022e64edca
mramani-pg <mramani1436@gmail.com>
2026-02-05 12:33:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,26 +5,37 @@ require("dotenv").config();
 const connectDB = require("./config/db");
 
 const app = express();
+
+
 connectDB();
 
-app.use(cors({
-  origin: "https://siyaram-boys-pg.vercel.app",
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true
-}));
-
-app.options("*", cors());
 
 app.use(express.json());
 
+
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:3000",
+      "https://siyaram-boys-pg.vercel.app"
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true
+  })
+);
+
+
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/rooms", require("./routes/roomRoutes"));
+
 
 app.get("/", (req, res) => {
   res.send("API Running...");
 });
 
+// ENV Check
 console.log("JWT:", process.env.JWT_SECRET ? "LOADED" : "MISSING");
 
 module.exports = app;
