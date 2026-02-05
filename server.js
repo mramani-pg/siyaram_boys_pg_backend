bde70f4f22cdb46822024b6bc7a030ced69d6997
mramani-pg <mramani1436@gmail.com>
2026-02-05 10:25:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,12 @@ const connectDB = require("./config/db");
 const app = express();
 connectDB();
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    "https://siyaram-boys-pg.vercel.app" 
+  ], 
+  methods: ["GET", "POST", "PUT", "DELETE"], credentials: true 
+}));
 app.use(express.json());
 
 app.use("/api/auth", require("./routes/authRoutes"));
