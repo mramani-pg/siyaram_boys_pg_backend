b5dd8ae4686a5147c7b751567f2dca4fdba9fc6e
mramani-pg <mramani1436@gmail.com>
2026-07-14 20:58:03 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -5,15 +5,15 @@ const userSchema = new mongoose.Schema(
   {
     username: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    role: { type: String, default: "admin" }
+    role: { type: String, default: "admin" },
   },
   { timestamps: true }
 );
 
-userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next();
+userSchema.pre("save", async function () {
+  if (!this.isModified("password")) return;
+
   this.password = await bcrypt.hash(this.password, 10);
-  next();
 });
 
 module.exports = mongoose.model("User", userSchema);
