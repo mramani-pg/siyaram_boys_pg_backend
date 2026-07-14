2645ca21aa0f4ae62b49c5a23454adcbac70a75c
mramani-pg <mramani1436@gmail.com>
2026-07-14 20:11:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -13,17 +13,22 @@ connectDB();
 app.use(express.json());
 
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  "http://localhost:3000",
+  "https://siyaram-boys-pg.vercel.app",
+];
+
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "http://localhost:3000",
-      "https://siyaram-boys-pg.vercel.app",
-      "https://siyaram-boys-pg.vercel.app/admin"
-    ],
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
   })
 );
 
